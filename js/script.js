d26682c487caf4649b6915aa094e510a19c181fb
vanjama <91538702+vanjama@users.noreply.github.com>
2022-01-30 17:39:16 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,24 @@ const cats = [
     }
 ];
 
+//Question1
+const cat = {complain:
+    function(){
+      console.log("Meow!")
+    }
+    };
+    cat.complain();
+    //Question2
+    const heading = document.querySelector("h3");
+    heading.innerHTML = "Updated heading";
+    //Question3
+    const headings = document.querySelectorAll("h3");
+    
+    for(let i = 0; i < headings.length; i++) {
+        headings[i].style.font = "2em";
+    }
+    //Question4
+    const heading = document.querySelector("subheading");
+    content.innerHTML += "subheading";
+    
+
